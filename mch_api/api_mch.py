a61446b982e3f5820e22606e641a0a174beb66fe
Faysal Ishtiaq Rabby <f.i.rabby@gmail.com>
2021-12-03 17:29:42 +0600

    modify requirements.txt

--- mch_api/api_mch.py
+++ mch_api/api_mch.py
@@ -20,7 +20,7 @@ app.config['MYSQL_USER'] = os.getenv('MCH_DB_USER')
 app.config['MYSQL_PASSWORD'] = os.getenv('MCH_DB_PASSWORD')
 app.config['MYSQL_DB'] = os.getenv('MCH_DB_NAME')
 app.config['MYSQL_PORT'] = int(os.getenv('MCH_DB_PORT'))
-app.config['SECRET_KEY'] = '7110c8ae51a4b5af97be6534caef90e4bb9bdcb3380af008f90b23a5d1616bf319bc298105da20fe'
+app.config['SECRET_KEY'] = os.getenv("APP_SECRET")
 mysql = MySQL(app)
 api = Api(app)
 
