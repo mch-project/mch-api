ee53b4bb530177650816bdbe3aab28e3ce4b0a93
Faysal Ishtiaq Rabby <f.i.rabby@gmail.com>
2021-12-03 17:29:42 +0600

    modify requirements.txt

--- setup.py
+++ setup.py
@@ -1,17 +1,12 @@
 from setuptools import setup, find_packages
 
+
+with open("requirements.txt", "r") as requirements:
+    install_requires = [line for line in requirements.readlines()]
+
 setup(
     name='mch_api',
     version='0.1.0',
     packages=find_packages(),
-    install_requires=[
-        "aniso8601==8.0.0",
-        "click==7.1.2",
-        "Flask==1.1.2",
-        "Flask-RESTful==0.3.8",
-        "Jinja2==2.11.2",
-        "flask-mysqldb==0.2.0",
-        "pandas>=1.2.2",
-        "markdown==2.6.11"
-    ]
+    install_requires=install_requires
 )
